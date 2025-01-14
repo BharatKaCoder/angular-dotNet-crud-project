bb1f7942d9461b5d53ef86fdbc9a44001c3ba2bd
khup practice <khup.practice@gmail.com>
2025-01-14 14:00:52 +0530

    code refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../Shared-Component/navbar/navbar.component';
+import { LoginComponent } from '../Shared-Component/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-dotNet-crud-project';
+  title = 'Angular19';
 }
